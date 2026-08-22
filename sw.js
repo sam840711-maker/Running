529579d72034e6278801a5cf688b5d421827fd40
sam840711-maker <sam840711@gmail.com>
2026-08-23 07:54:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SH Running Coach · minimal offline app-shell cache */
-const CACHE = 'shrc-v9';
+const CACHE = 'shrc-v10';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
